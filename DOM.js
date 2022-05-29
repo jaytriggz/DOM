e86bc02e8c25294a3bce6a504ce6a3aafffae736
Triggz <arotroy01@hotmail.com>
2022-05-29 20:28:41 +0100

    posting in codes

---
+++ DOM.js
@@ -0,0 +1,15 @@
+//examine the document object //
+//console.log(document.URL);
+console.log(document.title);
+
+//console.log(document.forms);
+
+//console.log(document.getElementById('header-title'));
+
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+headerTitle.innerText = 'world';
+
+//sunday Git learning
+//lets go again
