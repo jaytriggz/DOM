9eabe4ae795df79e9a6bffc6038af1357f6ffa80
Triggz <arotroy01@hotmail.com>
2022-05-31 01:37:47 +0100

    #Coding247
    stay focus.. deola makun

--- DOM.js
+++ DOM.js
@@ -1,16 +1,23 @@
 //examine the document object //
 //console.log(document.URL);
-console.log(document.title);
+//console.log(document.title);
 
 //console.log(document.forms);
 
 //console.log(document.getElementById('header-title'));
 
-var headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
-headerTitle.innerText = 'world';
+//var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+//headerTitle.innerText = 'world';
 
+//console.log("Hello world");
+//console.log(document.links);
+
+//GetElementById //
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
 //sunday Git learning
 //lets go again
 //i finally made it!! 
